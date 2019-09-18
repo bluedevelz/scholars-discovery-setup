8a329fede648d6f294e4145ae83b52537695e548
Rob Nelson <robert.nelson@duke.edu>
2019-09-18 14:35:04 -0400

    found a sparql to find publications for florida data - but does not find them for openvivo

--- data-importer/src/main/kotlin/main.kt
+++ data-importer/src/main/kotlin/main.kt
@@ -95,6 +95,30 @@ fun importData(dataset: String) {
     }
 }
 
+/*
+this doesn't work (with florida data):
+
+  <${uri}> core:relatedBy ?authorship .
+  ?authorship vitro:mostSpecificType ?authorshipType .
+  ?authorship core:relates ?publication .
+  ?publication a obo:IAO_0000030 .
+  ?publication rdfs:label ?label .
+
+ */
+fun findPublicationsSparql(uri: String): String {
+    val pubSparql = """
+      PREFIX vivo: <http://vivoweb.org/ontology/core#>
+      PREFIX obo: <http://purl.obolibrary.org/obo/>
+      PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+      SELECT * WHERE {
+        <$uri> vivo:authorInAuthorship ?authorship .
+        ?authorship vivo:linkedInformationResource ?publication .
+        ?publication rdfs:label ?title .
+      }
+    """
+    return pubSparql
+}
 // http://xmlns.com/foaf/0.1/Person
 fun listPeople(dataset: String) {
     println("trying to run query: $dataset")
@@ -102,20 +126,52 @@ fun listPeople(dataset: String) {
 
     println("dataset empty?=${connector.ds.isEmpty}")
 
+    // florida: 177449
+    /*
     val sparql = """
         PREFIX foaf:     <http://xmlns.com/foaf/0.1/>
 
-        SELECT * WHERE {
+        SELECT (count(distinct(?x)) as ?personcount)
+        WHERE {
             ?x a foaf:Person .
-            ?x ?p ?y .
-        } ORDER BY ?x LIMIT 100
+        }
+    """
+    */
+
+    val sparql = """
+        PREFIX foaf:     <http://xmlns.com/foaf/0.1/>
+        PREFIX vivo: <http://vivoweb.org/ontology/core#>
+        PREFIX bibo: <http://purl.org/ontology/bibo/>
+        PREFIX vitro: <http://vitro.mannlib.cornell.edu/ns/vitro/0.7#>
+
+        SELECT ?x
+        WHERE {
+            ?x a foaf:Person .
+        }
+        LIMIT 1000
     """
 
     connector.use { c ->
         val qe = c.query(sparql)
         qe.context.set(TDB.symUnionDefaultGraph, true)
         val results: ResultSet = qe.execSelect()
-        ResultSetFormatter.out(results)
+        for (r in results) {
+            println(r)
+            val uri = r.get("x")
+            val pubSparql = findPublicationsSparql(uri.toString())
+
+            val qe2 = c.query(pubSparql)
+            // need to set this everytime?
+            qe2.context.set(TDB.symUnionDefaultGraph, true)
+            val results2: ResultSet = qe2.execSelect()
+            println("uri=${uri}")
+            for (p in results2) {
+                //println(p)
+                val pub = p.get("title")
+                println("____pub->${pub}")
+            }
+        }
+        //ResultSetFormatter.out(results)
     }
 }
 
